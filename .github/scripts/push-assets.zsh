80fddf8b38b154b8a62a4feb3b4d8c5765134da5
Amonsuzuki <amonsuzuki@keio.jp>
2024-11-26 11:38:44 +0900

    zsh

---
+++ .github/scripts/push-assets.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+set -uex
+set -o pipefail
+
+git config user.name 'GitHub Action'
+git config user.email 'action@github.com'
+git add assets
+# for debug
+git status
+git diff --staged --quiet assets || {
+	git commit -m 'Update metrics [skip ci]'
+	git push
+}
