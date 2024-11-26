65c15b6e8644cc05f7d00318f15c57c4c18ac5e6
GitHub Action <action@github.com>
2024-11-26 14:35:58 +0900

    add

--- push-assets.bash
+++ push-assets.bash
@@ -6,10 +6,11 @@ set -o pipefail
 git config user.name 'GitHub Action'
 git config user.email 'action@github.com'
 git add assets
+git add push-assets.bash
 # for debug
 git status
 git diff --staged --quiet assets || {
 	git commit -m 'Update metrics [skip ci]'
-	git pull origin main --ff-only
+	git pull origin main --rebase
 	git push
 }
